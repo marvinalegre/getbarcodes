b1181b2607a66cc1bc442d52a02fbeecd22d721e
Marvin Alegre <marvinalegredev@gmail.com>
2024-10-30 15:29:38 +0800

    chore: add d1 db

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,26 +1,37 @@
 import jwt from "jsonwebtoken";
 
 export async function onRequestPost(context) {
-  const { request } = context;
-
+  const { request, env } = context;
   const { username, password } = await request.json();
+  const { results: users } = await env.DB.prepare(
+    "SELECT username, password FROM users WHERE username = ?"
+  )
+    .bind(username)
+    .all();
 
-  if (username === "marvin" && password === "foobar") {
-    const token = jwt.sign({ username: username }, context.env.JWT_SECRET, {
-      algorithm: "HS256",
-    });
-
-    return new Response(JSON.stringify({ success: true }), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Set-Cookie": `token=${token}; Path=/; HttpOnly; Secure; SameSite=Strict`,
-      },
-    });
-  } else {
+  if (users.length !== 1) {
     return Response.json({
       success: false,
       err: "Invalid username or password",
     });
   }
+
+  if (users[0].password !== password) {
+    return Response.json({
+      success: false,
+      err: "Invalid username or password",
+    });
+  }
+
+  const token = jwt.sign({ username: username }, env.JWT_SECRET, {
+    algorithm: "HS256",
+  });
+
+  return new Response(JSON.stringify({ success: true }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Set-Cookie": `token=${token}; Path=/; HttpOnly; Secure; SameSite=Strict`,
+    },
+  });
 }
