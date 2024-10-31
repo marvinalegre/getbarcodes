687bf269d4ca67b2909009a1a35912d2e8d82f77
Marvin Alegre <marvinalegredev@gmail.com>
2024-10-31 09:47:25 +0800

    fix: use auth middleware on user endpoint

--- functions/api/user.js
+++ functions/api/user.js
@@ -1,18 +1,6 @@
-import jwt from "jsonwebtoken";
-import { getCookies } from "../../utils/cookie";
-
 export async function onRequest(context) {
-  const { request } = context;
-
-  const { token } = getCookies(request);
-
-  try {
-    const decodedPayload = jwt.verify(token, context.env.JWT_SECRET, {
-      algorithms: ["HS256"],
-    });
-    return Response.json({ loggedIn: true, username: decodedPayload.username });
-  } catch (err) {
-    console.log(err.message);
-    return Response.json({ loggedIn: false });
+  if (context.data.username) {
+    return Response.json({ loggedIn: true, username: context.data.username });
   }
+  return Response.json({ loggedIn: false });
 }
